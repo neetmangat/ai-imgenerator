f39fb7b629e62d253ef0a449146cc5098681e07a
neetmangat <neetmangat@gmail.com>
2023-11-16 00:01:00 -0800

    created site header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.css";
+import "@/styles/globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " bg-gray-300"}>
+        {/* Header */}
+        <Header />
+
+        {/* Prompt Input */}
+
+        {/* Page Content */}
+
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="flex justify-between sticky p-5 top-0 bg-gray-100 z-50 shadow-md">
+      <div className="flex space-x-2 items-center">
+        <Image
+          src="https://links.papareact.com/4t3"
+          alt="logo"
+          height={30}
+          width={30}
+        />
+
+        <div className="">
+          <h1 className="font-bold">
+            The
+            <span className="text-violet-500"> IMG</span>
+            enerator
+          </h1>
+          <h2 className="text-xs">
+            Powered by DALL·E 2, ChatGPT & Microsoft Azure!
+          </h2>
+        </div>
+      </div>
+
+      <div className="flex text-xs md:text-base divide-x items-center text-gray-500">
+        <Link href={""} className="px-2 font-light text-right">
+          Porfolio
+        </Link>
+        <Link
+          href={"https://github.com/neetmangat/ai-image-generator"}
+          className="px-2 font-light"
+        >
+          GitHub Repo
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
